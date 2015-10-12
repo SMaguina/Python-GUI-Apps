6de860756bafdc5eb93222277d19ec4e1cbca4ec
SMaguina <smag23@gmail.com>
2015-10-11 18:45:46 -0700

    Import Files Script
    -Program script to automate importing internal files and print the file
    path directories into shell using shutil module

---
+++ importfiles.py
@@ -0,0 +1,13 @@
+import shutil
+
+shutil.move('/Users/Sylvia/Desktop/Folder A/Document1.txt','/Users/Sylvia/Desktop/Folder B')
+print 'Users/Sylvia/Desktop/Folder A/Document1.txt MOVED TO /Users/Sylvia/Desktop/Folder B'
+
+shutil.move('/Users/Sylvia/Desktop/Folder A/Document2.txt','/Users/Sylvia/Desktop/Folder B')
+print 'Users/Sylvia/Desktop/Folder A/Document2.txt MOVED TO /Users/Sylvia/Desktop/Folder B'
+
+shutil.move('/Users/Sylvia/Desktop/Folder A/Document3.txt','/Users/Sylvia/Desktop/Folder B')
+print 'Users/Sylvia/Desktop/Folder A/Document3.txt MOVED TO /Users/Sylvia/Desktop/Folder B'
+
+shutil.move('/Users/Sylvia/Desktop/Folder A/Document4.txt','/Users/Sylvia/Desktop/Folder B')
+print 'Users/Sylvia/Desktop/Folder A/Document4.txt MOVED TO /Users/Sylvia/Desktop/Folder B'
