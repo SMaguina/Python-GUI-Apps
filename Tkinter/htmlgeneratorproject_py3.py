6277e0159d772950486da6607e72c2ea4cc08985
SMaguina <smag23@gmail.com>
2015-11-01 20:01:57 -0800

    Final draft script

--- Tkinter/htmlgeneratorproject_py3.py
+++ Tkinter/htmlgeneratorproject_py3.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import webbrowser
 import sqlite3
@@ -9,74 +9,91 @@ cursor = db.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS content(id INTEGER PRIMARY KEY, name TEXT)''')
 db.commit()
 
-wbGui = Tk()
-source = StringVar()
+#Add entry field text into listbox
+def add_item():
+    listbox1.insert(tk.END, enter1.get())
 
-#Add record entry into listbox and database
-def addRecord():
-    name_i = source.get()
-    con = sqlite3.connect('storagetest.db')
-    with con:
-        cursor = con.cursor()
-        cursor.execute('''INSERT INTO content(name) VALUES(?)''', (name_i,))
-        lis = cursor.execute('''SELECT name FROM content''')
-        for item in lis:
-            list.insert(END, item)
+#Delete listbox selection    
+def delete_item():
+    try:
+        # get selected line index
+        index = listbox1.curselection()[0]
+        listbox1.delete(index)
+    except IndexError:
+        pass
 
-        con.commit()
-    print("Record added")
-    con.close()
+#Bind listbox selection into entry field 
+def get_list(event):
+    index = listbox1.curselection()[0]
+    seltext = listbox1.get(index)
+    enter1.delete(0, 50)
+    enter1.insert(0, seltext)
+
+#Bind entry text into listbox    
+def set_list(event):
+    try:
+        index = listbox1.curselection()[0]
+        listbox1.delete(index)
+    except IndexError:
+        index = tk.END
+    listbox1.insert(index, enter1.get())
 
 #Fetch all database records to display in listbox
-def fetchRecord():
+def sort_list():
     cont = sqlite3.connect('storagetest.db')
     with cont:
         cursort = cont.cursor()
         list_loadr = cursort.execute('''SELECT name FROM content''')
         list_load = list_loadr.fetchall()
         for item in list_load:
-            list.insert(END, item)
+            listbox1.insert(tk.END, item)
             
         cont.commit()
         
-#Load is supposed to select content choice and insert into text field
-def loadRecord():
-    name_i = source.get()
-    cont = sqlite3.connect('storagetest.db')
-    c = cont.cursor()
-    c.execute('SELECT * FROM content')
-    for item in c:
-        list.get(list.curselection())
-    cont.commit()
-    print ("select")
-        
-#Open web browser window and display selection in HTML
-def wbbrowser():
+#Open web browser window and display selection in HTML        
+def save_list():
     f = open('index.html','w')
     text = source.get()
     message = "<html><head></head><body><p>%s</p></body></html>" % text
     f.write(message)
     f.close()
     webbrowser.open_new_tab('index.html')
-    
-        
-wbGui.geometry('450x450+500+300')
-wbGui.title('Web Browser')
-wblabel = Label(wbGui,text='Type Your Text Below').pack()
-wbEntry = ttk.Entry(wbGui,textvariable=source).pack()
 
-#Row of buttons
-frame2 = Frame(wbGui)
-frame2.pack()
-wbbutton1 = Button(frame2,text="Add",command = addRecord)
-wbbutton2 = Button(frame2,text="Load",command = loadRecord)
-wbbutton3 = Button(frame2,text="Fetch",command = fetchRecord)
-wbbutton4 = Button(frame2,text="Open Browser",command = wbbrowser)
-wbbutton1.pack(side=LEFT); wbbutton2.pack(side=LEFT)
-wbbutton3.pack(side=LEFT); wbbutton4.pack(side=LEFT)
-
-list = Listbox(wbGui)
+root = tk.Tk()
+root.title("HTML Web Page Generator")
+source = tk.StringVar()
 
 
-list.pack()
-wbGui.mainloop()
+listbox1 = tk.Listbox(root, width=50, height=6)
+listbox1.grid(row=0, column=0)
+
+
+#Vertical scrollbar to the right of the listbox
+yscroll = tk.Scrollbar(command=listbox1.yview, orient=tk.VERTICAL)
+yscroll.grid(row=0, column=1, sticky=tk.N+tk.S)
+listbox1.configure(yscrollcommand=yscroll.set)
+ 
+
+enter1 = tk.Entry(root, textvariable=source, width=50)
+enter1.insert(0, 'Click on Fetch Button, then select item in the listbox')
+enter1.grid(row=1, column=0)
+
+enter1.bind('<Return>', set_list)
+enter1.bind('<Double-1>', set_list)
+
+button1 = tk.Button(root, text='Fetch DB List    ', command=sort_list)
+button1.grid(row=2, column=0, sticky=tk.W)
+
+button2 = tk.Button(root, text='Open Browser  ', command=save_list)
+button2.grid(row=3, column=0, sticky=tk.W)
+
+button3 = tk.Button(root, text='Add entry text to listbox', command=add_item)
+button3.grid(row=2, column=0, sticky=tk.E)
+
+button4 = tk.Button(root, text='Delete selected line     ', command=delete_item)
+button4.grid(row=3, column=0, sticky=tk.E)
+ 
+
+listbox1.bind('<ButtonRelease-1>', get_list)
+ 
+root.mainloop()
