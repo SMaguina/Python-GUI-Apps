d3d24b5e8d8d940ac4c2161a0e5e20afbf4c28bd
SMaguina <smag23@gmail.com>
2015-11-10 20:48:21 -0800

    Clock Drill
    Clock Python Program to Calculate Time Lived Based on Age

---
+++ Coding Practicals/clock.py
@@ -0,0 +1,8 @@
+print("Let's see how long you have lived in days, minutes and seconds.")
+name = input("name: ")
+print("now enter your age")
+age = int(input("age: "))
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+print(name, "has been alive for", days,"days", minutes, "minutes and", seconds, "seconds! Wow!")
